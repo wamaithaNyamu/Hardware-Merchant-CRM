950ecea2a27decdb586194c11e626dc9ab82e8b6
Wamaitha Nyamu <wamaithanyamu@gmail.com>
2020-07-20 21:06:37 +0300

    made templates for our views

--- apt3090/main/urls.py
+++ apt3090/main/urls.py
@@ -6,5 +6,7 @@ app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("/customer", views.homepage, name="customer"),
+    path("/hrpage", views.homepage, name="homepage"),
 
 ]
--- apt3090/main/views.py
+++ apt3090/main/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def homepage(request):
-    return HttpResponse("pythonprogramming.net homepage! Wow so #amaze.")
+# landing page view
+# landing page for the merchants business - hardware business
+# login and logout buttons and register buttons
+def landingpage(request):
+    return HttpResponse("APT 3090 GROUP WORK!")
+
+# customer view
+# owner of a retail hadware
+# only adds their credit card information once
+# can (R) make purchases
+# can see previous purchases
+# only one who can CRUD their credit card info
+# can make an order from products view
+def customer(request):
+    return HttpResponse("APT 3090 GROUP WORK!")
+
+# intern view
+# can only see customer insensitive info
+# can RU orders - change order status as delivered, cancelled, returns
+def intern(request):
+    return HttpResponse("APT 3090 GROUP WORK!")
+
+# supervisor view
+# supervisor can see everything
+# can CRUD interns
+# can CRUD products
+# can CRUD customer
+# can see customers insensitive info
+#name,order, email, hashed creditcard info
+# can delete customer
+# can add customer
+# send invoice to the customer
+# CRU invoices - only the superuser can delete invoices
+def supervisor(request):
+    return HttpResponse("APT 3090 GROUP WORK!")
+
+
+# products view
+# shows products and their prices and ability to place an order
+def products(request):
+    return HttpResponse("APT 3090 GROUP WORK!")
