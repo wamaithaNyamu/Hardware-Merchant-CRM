d3a4233562dedc26dda6f4a3eca86f566381906b
Wamaitha Nyamu <wamaithanyamu@gmail.com>
2020-07-21 19:07:16 +0300

    user cant go to register or g=login if theyre logged in

--- apt3090/main/views.py
+++ apt3090/main/views.py
@@ -230,14 +230,12 @@ def registerPage(request):
 
 def loginPage(request):
     if request.user.is_authenticated:
-        return redirect('main:login')
+        return redirect('main:supervisor')
     else:
         if request.method == 'POST':
             username = request.POST.get('username')
             password = request.POST.get('password')
-
             user = authenticate(request, username=username, password=password)
-
             if user is not None:
                 login(request, user)
                 return redirect('main:supervisor')
