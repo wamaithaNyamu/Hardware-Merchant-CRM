de765078dda813f87e85b672eb03420b01d149fc
Wamaitha Nyamu <wamaithanyamu@gmail.com>
2020-07-21 22:53:43 +0300

    updates working

--- apt3090/main/models.py
+++ apt3090/main/models.py
@@ -9,7 +9,12 @@ class Customer(models.Model):
     email = models.CharField(max_length=200,  null=True)
     phone = models.CharField(max_length =200, null=True)
     date_created = models.DateTimeField(auto_now_add=True,  null=True)
-    
+    # AES ENCRYPTION PART
+    credit_card_number_encrypted = models.CharField(max_length=100, null=True)
+    cvv_encrypted = models.CharField(max_length=4,null=True)
+    expiry_month = models.CharField(max_length=3,null=True)
+    expiry_year = models.CharField(max_length=3,null=True)
+
     def __str__(self):
         return self.name
 
